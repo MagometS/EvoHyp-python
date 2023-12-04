7dcc232caaa7e6c3657118b6b5b7358d05ff3c3c
MagometS <magik200219@gmail.com>
2023-12-05 00:02:54 +0300

    добавил parse_fitness

--- DEEP/DEEP.py
+++ DEEP/DEEP.py
@@ -1,2 +1,60 @@
+import os
+import subprocess
+from Heuristics import Heuristics
+
+
 class DEEP:
-    pass
+    filename = None  # Class attribute to hold the filename
+
+    def __init__(self, filename):
+        DEEP.filename = filename
+
+    def get_solution(self):
+        return [5.0, 10.0, 16.0]
+
+    def parsed_answer(log_file, task_size):
+        DEEP.x[0] = 5.0
+        DEEP.x[1] = 10.0
+        DEEP.x[2] = 16.0
+
+    def create_solution(self, heuristics):
+        sections = [f"default_settings{i}" for i in range(15)]  # Creating section names
+        fit = " "  # Initializing fit
+
+        heuristics_map = {
+            'A': "default_settings1",
+            'B': "default_settings2",
+            'C': "default_settings3",
+            'D': "default_settings4",
+            'E': "default_settings5",
+            'F': "default_settings6",
+            'G': "default_settings7",
+            'H': "default_settings9",
+            'I': "default_settings10",
+            'J': "default_settings8",
+            'K': "default_settings11",
+            'L': "default_settings12",
+            'M': "default_settings13",
+            'N': "default_settings14"
+        }
+
+        for c in heuristics:
+            heuristics_obj = Heuristics()  # creating Heuristics object
+            section_name = heuristics_map[c]  # getting section name from map
+            fit = heuristics_obj.run_heuristic(self.filename, section_name)  # calling run_heuristic
+
+        return fit
+
+    def parse_fitness(log_file):
+        fit = 0.0
+        try:
+            with open(log_file, 'r') as file:
+                log_line = file.readline()
+                print(log_line)
+                parts = log_line.split("score:")
+                score_parts = parts[1].split(" ")
+                fit = float(score_parts[0])
+        except Exception as e:
+            print(e)
+        return fit
+
--- DEEP/DeepSolution.py
+++ DEEP/DeepSolution.py
@@ -3,33 +3,25 @@ from typing import List
 from GeneticAlgorithm.Solution import Solution
 from DEEP import DEEP
 
+
 class DeepSolution(Solution):
-
-
     heuristic_combination: str = ''
-
 
     fitness: float
 
-
     initial_solution: List[str] = []
-
 
     def get_fitness(self) -> float:
         return self.fitness
-    
 
     def set_heuristic_combination(self, heuristic_combination: str):
         self.heuristic_combination = heuristic_combination
 
-
     def get_heuristic_combination(self) -> str:
         return self.heuristic_combination
-    
 
     def get_solution(self) -> List[str]:
         return self.initial_solution
-    
 
     def fitter(self, other: Solution):
         if other.get_fitness() >= self.fitness:
@@ -38,7 +30,6 @@ class DeepSolution(Solution):
             return -1
         else:
             return 0
-
 
     def create_solution(self, filename):
         sol = DEEP(filename)
@@ -50,4 +41,3 @@ class DeepSolution(Solution):
             self.solution = fitline
         except OSError as e:
             print(e)
-            
