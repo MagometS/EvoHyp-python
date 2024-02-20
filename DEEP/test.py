35d65348a3a094f605cf677d7e963af80fd959b9
MagometS <magik200219@gmail.com>
2024-02-20 03:33:07 +0300

    тест

--- DEEP/test.py
+++ DEEP/test.py
@@ -1,7 +1,10 @@
 from DeepSolution import DeepSolution
+from Heuristics import Heuristics
 '''
 ds = DeepSolution()
 ds.set_heuristic_combination("ABCD")
 str = ds.get_heuristic_combination()
 print(str)
 '''
+heuristics_obj = Heuristics()
+heuristics_obj.run_heuristic('deep_rastr.ini')
