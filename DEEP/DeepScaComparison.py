d6c03723579a13918b7e658ec01f35cb1f936c50
Magomet <magik200219@gmail.com>
2024-03-18 08:11:52 +0300

    создал DeppScaComparison class

---
+++ DEEP/DeepScaComparison.py
@@ -0,0 +1,8 @@
+from DeepProblem import DeepProblem
+from ScaProblem import ScaProblem
+
+class DeepScaComparison:
+    heuristics = None
+
+    def __init__(self, heuristics, filename_deep, parametrs) :
+        pass
