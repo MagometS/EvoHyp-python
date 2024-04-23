d2cef3670d6bdd530f20adf9f5c8ad2808ac55d0
Magomet <magik200219@gmail.com>
2024-04-23 10:27:06 +0300

    В SCA добалена _obj_function()

--- DEEP/SCA.py
+++ DEEP/SCA.py
@@ -1,15 +1,17 @@
 import numpy as np
 import configparser
+import subprocess
+import io
 
 class SCA:
 
-    obj_function = None
+    #obj_function = None
     file_name = None
     
 
     def __init__(self, file_name, obj_function):
         self.file_name = file_name
-        self.obj_function = obj_function
+        #self.obj_function = obj_function
         
 
     def run_heuristic(self, section_name):
@@ -34,7 +36,7 @@ class SCA:
 
         for _ in range(max_iterations):
             for agent in population:
-                fitness = self.obj_function(agent)
+                fitness = self._obj_function(agent)
                 if fitness < best_fitness:
                     best_fitness = fitness
                     best_solution = agent
@@ -47,3 +49,37 @@ class SCA:
                 agent = np.minimum(np.maximum(agent, lower_bound), upper_bound)
 
         return best_fitness
+    
+    def _obj_function(self, agent):
+        try:
+    
+            command = './rastrign_func.py'                      #формируем команду для subprocess
+            arg1 = str(1.56)
+            arg2 = str(2.67)
+            arg3 = str(7.87)
+            arguments = [command, arg1, arg2, arg3]
+
+            p = subprocess.run(arguments, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  
+            stdout_text = io.TextIOWrapper(io.BytesIO(p.stdout), encoding='utf-8') #оборачиваем p.stdout чтобы работал readline()
+
+            while True:
+                line = stdout_text.readline().strip()
+                if line == '':
+                    break
+                log_line = line
+
+            # part1 = log_line.split("cost:")
+            # # print(part1[1])
+            # # p.communicate()
+            # part = part1[1]
+            part = log_line
+
+        except Exception as e:
+            print(e)
+
+        return part
+
+    
+        
+
